bf6aa82edfa9cb280c7aba438383388b9c99e238
aryanbhatt767 <aryanbhatt767@gmail.com>
2026-06-02 11:36:06 +0530

    Add folder structure

---
+++ Week_1/Day_1/Best_Time_Buy_Sell_Stock.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+
+        int maxi=0;
+
+        int mini=prices[0];
+
+        for(int i=1;i<prices.length;i++) {
+            maxi=Math.max(maxi,prices[i]-mini);
+            mini=Math.min(mini,prices[i]);
+        }
+        return maxi;
+        
+    }
+}
